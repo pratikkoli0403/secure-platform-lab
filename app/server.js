786391484f2c5d618359ac95607573d815c1792b
Pratik Koli <pratikkoli0403@gmail.com>
2026-09-15 13:27:44 +0000

    Harden application security and logging

--- app/server.js
+++ app/server.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 
 const app = express();
-
+app.use(helmet());
+app.use(morgan('combined'));
 app.get('/', (req, res) => {
   res.send('Secure Platform Lab Running');
 });
