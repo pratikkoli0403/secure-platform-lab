ef0459ab0d4340aa3d50323082847fb0fb776af8
Pratik Koli <pratikkoli0403@gmail.com>
2026-09-17 14:36:09 +0000

    Add Prometheus application monitoring

--- app/server.js
+++ app/server.js
@@ -1,10 +1,27 @@
 const express = require('express');
 const helmet = require('helmet');
 const morgan = require('morgan');
+const client = require('prom-client');
 
 const app = express();
+const register = new client.Registry();
+
+client.collectDefaultMetrics({
+    register
+});
+const httpRequestsTotal = new client.Counter({
+  name: 'http_requests_total',
+  help: 'Total number of HTTP requests received',
+  registers: [register]
+});
 app.use(helmet());
 app.use(morgan('combined'));
+
+app.use((req, res, next) => {
+  httpRequestsTotal.inc();
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Secure Platform Lab Running');
 });
@@ -13,6 +30,26 @@ app.get('/health', (req, res) => {
   res.status(200).json({ status: 'ok' });
 });
 
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', register.contentType);
+  res.end(await register.metrics());
+});
+
+app.use((req, res) => {
+  res.status(404).json({
+      error: 'Not Found',
+      message: 'Route does not exist'
+  });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err);
+
+  res.status(500).json({
+      error: 'Internal Server Error',
+      message: 'Something went wrong'
+  });
+});
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
